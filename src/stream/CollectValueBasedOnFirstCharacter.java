aa548902ce89ca81af2371077f34716fa80ec4ea
Shubham <shubhamkumarhms13@gmail.com>
2025-08-06 00:09:57 +0530

    Practice Code

--- src/stream/CollectValueBasedOnFirstCharacter.java
+++ src/stream/CollectValueBasedOnFirstCharacter.java
@@ -7,10 +7,11 @@ import java.util.stream.Collectors;
 
 public class CollectValueBasedOnFirstCharacter {
     public static void main(String[] args) {
-        List<String> stringList = Arrays.asList("apple", "banana", "apricort", "carrot", "cart", "buffallo", "bucket","");
+        List<String> stringList = Arrays.asList("apple", "ban2ana", "apricort", "carrot", "ca4rt", "buffallo", "bucket","");
         Map<Character, List<String>> collect = stringList.stream()
                 .filter(str -> str != null && !str.isEmpty())
                 .map(String::trim)
+//                .filter(str -> str.matches("[a-zA-Z]+"))
                 .collect(Collectors.groupingBy(str -> str.charAt(0)));
         System.out.println(collect);
     }
