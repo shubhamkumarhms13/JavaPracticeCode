934aff7dba2c176971925fc02c1971e839af551e
Shubham <shubhamkumarhms13@gmail.com>
2025-08-07 22:47:09 +0530

    Practice Code

--- src/com/practice/interview/ConvertToLowerCase.java
+++ src/com/practice/interview/ConvertToLowerCase.java
@@ -17,7 +17,7 @@ public class ConvertToLowerCase {
     }
 
     public static void main(String[] args) {
-        String str = "Hello";
+        String str = "HELLO4FDGbvcbc";
         System.out.println(toLowerCase(str));
     }
 }
