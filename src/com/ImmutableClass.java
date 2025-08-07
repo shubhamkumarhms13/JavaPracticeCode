0079aadf531da27e5c6683b528a4c4c342f175cf
Shubham <shubhamkumarhms13@gmail.com>
2025-08-07 20:11:16 +0530

    Practice Code

---
+++ src/com/ImmutableClass.java
@@ -0,0 +1,38 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ImmutableClass {
+    private final int id;
+    private final String name;
+    private final List<Object> petNameList;
+
+    ImmutableClass(int id, String name, List<Object> petNameList) {
+        this.id = id;
+        this.name = name;
+        this.petNameList = new ArrayList<>(petNameList);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Object> getPetNameList() {
+        return new ArrayList<>(petNameList);
+    }
+
+    public static void main(String[] args) {
+        List<Object> names = new ArrayList<>();
+        names.add("Tommy");
+        names.add("Jerry");
+        names.add("Bhow");
+        ImmutableClass imc = new ImmutableClass(1, "Harry", names);
+        names.add("Meow");
+        System.out.println(imc.getPetNameList());
+    }
+}
