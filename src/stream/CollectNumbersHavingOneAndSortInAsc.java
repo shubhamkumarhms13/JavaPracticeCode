f63bbf20e8fbeedf1759dae6f9101e530893e0be
Shubham <shubhamkumarhms13@gmail.com>
2025-08-11 10:19:50 +0530

    Practice Code

--- src/stream/CollectNumbersHavingOneAndSortInAsc.java
+++ src/stream/CollectNumbersHavingOneAndSortInAsc.java
@@ -3,15 +3,15 @@ package stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class CollectNumbersHavingOneAndSortInAsc {
     public static void main(String[] args) {
         //Using collection
-        List<Integer> numbers = Arrays.asList(11, 22, 4, 14, 6, 45, 1, -15, 10, 21, 16, 31, 71, 0, null, 61);
+        List<Integer> numbers = Arrays.asList(11, 22, 4, 14, 6, 45, 1, -15, 10, 21, 16, 1, 31, 71, 0, null, 61);
         List<Integer> list1 = numbers.stream()
                 .filter(Objects::nonNull)
                 .filter(num -> String.valueOf(num).contains("1"))
+                .distinct()
                 .sorted()
                 .toList();
         System.out.println(list1);
@@ -21,6 +21,7 @@ public class CollectNumbersHavingOneAndSortInAsc {
         List<Integer> list = Arrays.stream(nums)
                 .boxed()
                 .filter(num -> String.valueOf(num).contains("1"))
+                .distinct()
                 .sorted()
                 .toList();
         System.out.println(list);
