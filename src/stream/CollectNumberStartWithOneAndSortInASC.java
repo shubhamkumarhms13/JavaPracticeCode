afff021f9c6d2debd62e00dda28c024d45671771
Shubham <shubhamkumarhms13@gmail.com>
2025-08-11 10:24:16 +0530

    Practice Code

--- src/stream/CollectNumberStartWithOneAndSortInASC.java
+++ src/stream/CollectNumberStartWithOneAndSortInASC.java
@@ -6,8 +6,23 @@ import java.util.Objects;
 
 public class CollectNumberStartWithOneAndSortInASC {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(12,21,32,41,14,15,1,16,19,21,13,null);
-        List<Integer> list = numbers.stream().filter(Objects::nonNull).filter(s -> String.valueOf(s).startsWith("1")).sorted().toList();
+        //With Collections
+        List<Integer> numbers = Arrays.asList(12, 21, 32, 1, 41, 14, 15, 1, 16, 19, 21, 13, null);
+        List<Integer> list = numbers.stream()
+                .filter(Objects::nonNull)
+                .filter(s -> String.valueOf(s).startsWith("1"))
+                .distinct()
+                .sorted().toList();
         System.out.println(list);
+
+        //Without Collections
+        int[] nums = {12, 21, 1, 32, 41, 14, 15, 1, 16, 19, 21, 13};
+        List<Integer> list1 = Arrays.stream(nums)
+                .boxed()
+                .filter(x -> String.valueOf(x).startsWith("1"))
+                .distinct()
+                .sorted()
+                .toList();
+        System.out.println(list1);
     }
 }
