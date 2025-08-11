c43f66c54e3e4ad32ba6538fd041f46e225f04d7
Shubham <shubhamkumarhms13@gmail.com>
2025-08-11 10:17:18 +0530

    Practice Code

--- src/stream/CollectNumEndWithOneAndSortInASC.java
+++ src/stream/CollectNumEndWithOneAndSortInASC.java
@@ -5,8 +5,21 @@ import java.util.List;
 
 public class CollectNumEndWithOneAndSortInASC {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(25,41,23,21,55,31,11,44,33,21,1,null);
-        List<Integer> list = numbers.stream().filter(x-> x != null).filter(n -> n % 10 == 1).distinct().sorted().toList();
+        List<Integer> numbers = Arrays.asList(25, 41, 23, 11, 21, 55, 31, 11, 44, 33, 21, 1, null);
+        List<Integer> list = numbers.stream()
+                .filter(x -> x != null)
+                .filter(n -> n % 10 == 1)
+                .distinct().sorted()
+                .toList();
         System.out.println(list);
+
+        int[] nums = {25, 41, 23, 21, 55, 31, 11, 44, 33, 21, 1};
+        List<Integer> list1 = Arrays.stream(nums)
+                .boxed()
+                .filter(x -> x % 10 == 1)
+                .distinct()
+                .sorted()
+                .toList();
+        System.out.println(list1);
     }
 }
